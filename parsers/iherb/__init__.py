3183a4635c674ba08931af933f8a0c36789bc101
ivymerfe <82306640+ivymerfe@users.noreply.github.com>
2025-05-02 12:59:59 +0700

    iherb category brand names

--- parsers/iherb/__init__.py
+++ parsers/iherb/__init__.py
@@ -1,101 +1,89 @@
-from rnet import Impersonate, Client
-from bs4 import BeautifulSoup
 import json
 import os
+import time
+
+from bs4 import BeautifulSoup
+from curl_cffi import AsyncSession
+
+from .data import scrap_categories
 
 async def scrap_essential():
   if not os.path.exists("data/iherb"):
     os.makedirs("data/iherb")
-  
-  client = Client(impersonate=Impersonate.Chrome134)
-  if not os.path.exists("data/iherb/brand_map.json"):
-    await get_all_brands(client)
-  
-  if not os.path.exists("data/iherb/categories.json"):
-    with open("data/iherb/brand_map.json", "r", encoding="utf-8") as f:
-      brand_map = json.load(f)
-    await scrap_category_list(client, scrap_categories[:1], brand_map)
+
+  async with AsyncSession() as client:
+    if not os.path.exists("data/iherb/brand_map.json"):
+      await get_all_brands(client)
+
+    if not os.path.exists("data/iherb/categories.json"):
+      with open("data/iherb/brand_map.json", "r", encoding="utf-8") as f:
+        brand_map = json.load(f)
+      await scrap_category_list(client, scrap_categories, brand_map)
 
 
 async def scrap_full():
   pass
 
 
-scrap_categories = [
-  {
-    "name": "Vitamins",
-    "url": "https://ru.iherb.com/c/supplements?cids=101072",
-  },
-  {
-    "name": "Bone, Joint & Cartilage",
-    "url": "https://ru.iherb.com/c/supplements?cids=100727",
-  },
-  {
-    "name": "Minerals",
-    "url": "https://ru.iherb.com/c/supplements?cids=1800",
-  },
-  {
-    "name": "Children's Health",
-    "url": "https://ru.iherb.com/c/supplements?cids=100349",
-  },
-  {
-    "name": "Amino Acids",
-    "url": "https://ru.iherb.com/c/supplements?cids=1694",
-  },
-  {
-    "name": "Brain & Cognitive",
-    "url": "https://ru.iherb.com/c/supplements?cids=105803",
-  },
-  {
-    "name": "Women's Health",
-    "url": "https://ru.iherb.com/c/supplements?cids=8741",
-  },
-  {
-    "name": "Fish Oil & Omegas (EPA DHA)",
-    "url": "https://ru.iherb.com/c/supplements?cids=1542",
-  },
-  {
-    "name": "Men's Health",
-    "url": "https://ru.iherb.com/c/supplements?cids=3282",
-  },
-  {
-    "name": "Phospholipids",
-    "url": "https://ru.iherb.com/c/supplements?cids=102094",
-  },
-]
-
-async def scrap_category_list(client: Client, categories: list, brand_map: dict):
+async def scrap_category_list(client: AsyncSession, categories: list, brand_map: dict):
   print("Scraping categories...")
   for category in categories:
-    brand_ids = await scrap_category(client, category, brand_map)
+    brand_names = await fetch_brand_names(client, category)
+    brand_ids = []
+    for brand_name in brand_names:
+      brand_id = brand_map.get(brand_name)
+      if brand_id is None:
+        print(f"Brand {brand_name} not found in brand map")
+        continue
+      brand_ids.append(brand_id)
     category["brands"] = brand_ids
-  
+    time.sleep(1)
+
   with open("data/iherb/categories.json", "w", encoding="utf-8") as f:
     json.dump(categories, f, ensure_ascii=False, indent=2)
 
-async def scrap_category(client: Client, category: dict, brand_map: dict):
-  print(f"Scraping category {category['name']} -> {category['url']}")
-  resp = await client.get(category["url"])
-  data = await resp.text()
-  brand_names = parse_brand_names(data)
-  print("Brand names found:", len(brand_names))
-  brand_ids = []
-  for brand_name in brand_names:
-    brand_id = brand_map.get(brand_name)
-    if brand_id is None:
-      print(f"{brand_name} not found in brand map")
-      continue
-    brand_ids.append(brand_id)
-  return brand_ids
+
+async def fetch_brand_names(client: AsyncSession, category: dict):
+  print(f"Get brand names for category {category['name']} -> {category['url']}")
+  resp = await client.post(
+    "https://catalog.app.iherb.com/category/v2/supplements/filters",
+    impersonate="chrome",
+    json={
+      "categoryIds": [category["category_id"]],
+      "healthTopicIds": [],
+      "attributeValueIds": [],
+      "brandCodes": [],
+      "priceRanges": [],
+      "ratings": [],
+      "weights": [],
+      "specials": "",
+      "sort": None,
+      "showShippingSaver": False,
+      "showITested": False,
+      "searchWithinKeyWord": "",
+      "programs": [],
+      "showOnlyAvailable": False,
+    },
+  )
+  json_data = json.loads(resp.text)
+  if "filters" not in json_data:
+    print("No filters found")
+    return []
+  brand_names = []
+  for filter_desc in json_data["filters"]:
+    if filter_desc["filterName"] == "Brands":
+      for filter_item in filter_desc["options"]:
+        brand_names.append(filter_item["displayName"])
+      break
+  return brand_names
 
 
-async def get_all_brands(client: Client):
+async def get_all_brands(client: AsyncSession):
   print("Getting all brands...")
-  resp = await client.get("https://www.iherb.com/catalog/brandsaz")
-  data = await resp.text()
-  brands = parse_brands_html(data)
+  resp = await client.get("https://ru.iherb.com/catalog/brandsaz", impersonate="chrome")
+  brands = parse_brands_html(resp.text)
   print("Brands found:", len(brands))
-  
+
   with open("data/iherb/brand_map.json", "w", encoding="utf-8") as f:
     json.dump(brands, f, ensure_ascii=False, indent=2)
 
@@ -109,13 +97,4 @@ def parse_brands_html(data: str):
     url = element.get("href")
     name = element.get_text(strip=True)
     result[name] = url.rpartition("/")[2]
-  return result
-
-
-def parse_brand_names(data: str):
-  soup = BeautifulSoup(data, "html.parser")
-  result = []
-  for element in soup.select("label[data-ga-event-action='brands']"):
-    brand_name = element.get("title")
-    result.append(brand_name)
   return result
---
+++ parsers/iherb/data.py
@@ -0,0 +1,53 @@
+
+scrap_categories = [
+  {
+    "name": "Витамины",
+    "url": "https://ru.iherb.com/c/supplements?cids=101072",
+    "category_id": "101072",
+  },
+  {
+    "name": "Кости, суставы и хрящи",
+    "url": "https://ru.iherb.com/c/supplements?cids=100727",
+    "category_id": "100727",
+  },
+  {
+    "name": "Микроэлементы",
+    "url": "https://ru.iherb.com/c/supplements?cids=1800",
+    "category_id": "1800",
+  },
+  {
+    "name": "Здоровье детей",
+    "url": "https://ru.iherb.com/c/supplements?cids=100349",
+    "category_id": "100349",
+  },
+  {
+    "name": "Аминокислоты",
+    "url": "https://ru.iherb.com/c/supplements?cids=1694",
+    "category_id": "1694",
+  },
+  {
+    "name": "Деятельность мозга",
+    "url": "https://ru.iherb.com/c/supplements?cids=105803",
+    "category_id": "105803",
+  },
+  {
+    "name": "Женское здоровье",
+    "url": "https://ru.iherb.com/c/supplements?cids=8741",
+    "category_id": "8741",
+  },
+  {
+    "name": "Рыбий жир и омега (ЭПК и ДГК)",
+    "url": "https://ru.iherb.com/c/supplements?cids=1542",
+    "category_id": "1542",
+  },
+  {
+    "name": "Здоровье мужчины",
+    "url": "https://ru.iherb.com/c/supplements?cids=3282",
+    "category_id": "3282",
+  },
+  {
+    "name": "Фосфолипиды",
+    "url": "https://ru.iherb.com/c/supplements?cids=102094",
+    "category_id": "102094",
+  },
+]
